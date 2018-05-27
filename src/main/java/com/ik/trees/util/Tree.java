157475ff201a47c499b134425b6b79d41ae9f95f
rpurigella <ramesh.purigella@boku.com>
2018-05-27 00:00:48 -0700

    Added to String to Node

--- src/main/java/com/ik/trees/util/Tree.java
+++ src/main/java/com/ik/trees/util/Tree.java
@@ -15,6 +15,16 @@ public class Tree {
         public Node(int value) {
             this.val = value;
         }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("Node{");
+            sb.append("val=").append(val);
+            sb.append(", left=").append(left);
+            sb.append(", right=").append(right);
+            sb.append('}');
+            return sb.toString();
+        }
     }
 
 
